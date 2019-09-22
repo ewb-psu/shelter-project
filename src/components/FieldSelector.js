a2df1784b936102832f5d9c2f0cda105c4b6175f
Nathaniel <nathaniels2016@gmail.com>
2019-09-22 14:45:39 -0700

    Changes to the selector_page component

--- src/components/FieldSelector.js
+++ src/components/FieldSelector.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import NavBlock from "./NavigationBlock";
-import Footer from "./Footer";
+import "../assets/FieldSelector.js.scss"
 
 class FieldSelector extends React.Component {
     render() {
         return(
         <div className={"FieldSelector"}>
+            <h1> Gender </h1>
             <div className={"GenderSelector"}>
                 <div className={"Male"}>Male</div>
-                <div className={"Female"}>Female</div>
                 <div className={"Transgender Male"}>Transgender Male</div>
+                <div className={"Female"}>Female</div>
+
                 <div className={"Transgender Female"}>Transgender Female</div>
             </div>
+            <h1> Age </h1>
             <div className={"AgeSelector"}>
+
                 <input type={"number"} value={"2"}/>
             </div>
+            <h1> Donde Estas? </h1>
             <div className={"Location"}>
                 <p>Zip Code: <input type={"Number"}/></p>
                 <a href={"google.com"}>Your location</a>
