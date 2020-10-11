0f23892c7bcceafef96c647850e8d138a8d48851
Mark <cybersyn@gmail.com>
2020-10-11 00:57:13 -0700

    added urls to nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,11 @@ const Nav = () => {
 	return (
 		<nav className='lg:w-4/5 lg:ml-auto'>
 			<ul className='mt-12 flex p-2 flex-wrap justify-center rounded border shadow text-sm'>
-				<Link to='/'>
 					{' '}
 					<li className='px-5 border  active:bg-themeTealFlat hover:bg-themeTeal'>
-						Home
+						<a href='https://www.211info.org/'>Home</a>
 					</li>
-				</Link>
-				<Link to='resources'>
+				<Link to='/resources'>
 					{' '}
 					<li className='px-5 border active:bg-themeTealFlat hover:bg-themeTeal'>
 						Find Resources
@@ -20,16 +18,24 @@ const Nav = () => {
 				</Link>
 
 				<li className='px-5 border active:bg-themeTealFlat hover:bg-themeTeal'>
+					<a href='https://www.211info.org/about'>
 					About Us
+					</a>
 				</li>
 				<li className='px-5 border active:bg-themeTealFlat hover:bg-themeTeal'>
+					<a href='https://www.211info.org/programs'>
 					Our Programs
+					</a>
 				</li>
 				<li className='px-5 border active:bg-themeTealFlat hover:bg-themeTeal'>
+					<a href='http://www.navigateresources.net/info/Request.aspx'>
 					For Providers
+					</a>
 				</li>
 				<li className='px-5 border active:bg-themeTealFlat hover:bg-themeTeal'>
+					<a href='https://www.211info.org/reporting'>
 					Our Data
+					</a>
 				</li>
 			</ul>
 		</nav>
